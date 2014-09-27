fc05a647e578df29432c105f7d9a9c545884154c
daloco13 <pickity.pickity.pick@gmail.com>
2014-09-27 21:24:37 +0800

    html to laravel forms on index.php

--- Airlines/app/views/content/index.blade.php
+++ Airlines/app/views/content/index.blade.php
@@ -11,9 +11,8 @@
 		</h1>
 	</div> <!-- /demo-headline -->
 
-	<h3 class="demo-panel-title">Navbar</h3>
       <div class="row demo-row">
-        <div class="col-xs-10">
+        <div class="col-xs-9">
           <nav class="navbar navbar-inverse navbar-embossed" role="navigation">
 
             <div class="collapse navbar-collapse" id="navbar-collapse-01">
@@ -29,72 +28,61 @@
       </div> <!-- /row -->
 
       <!-- put form here -->
+      {{ Form::open(['url'=>'/']) }}
 
       <div class='form-row'>
 		<div class='col-xs-2 form-group required'>
-			<label class='control-label'>Return</label>&nbsp;
-			<input type="radio" name="intTripType" id="intTripTypeReturn" onclick="disablefield()" value="return" checked="checked" />
+			{{ Form::radio('triptype','',['name'=>'intTripType', 'id'=>'intTripTypeReturn', 'onclick'=>'disablefield()', 'value'=>'roundtrip']) }}
+			{{ Form::label('return','Round Trip', ['class'=>'control-label']) }}
 		</div>
 		<div class='col-xs-2 form-group required'>
-			<label class='control-label'>One Way</label>&nbsp;
-			<input type="radio" name="intTripType" id="intTripTypeOneWay" onclick="disablefield()" value="oneway" />
+			{{ Form::radio('triptype','',['name'=>'intTripType', 'id'=>'intTripTypeOneWay', 'onclick'=>'disablefield()', 'value'=>'oneway']) }}
+			{{ Form::label('oneway','One Way', ['class'=>'control-label']) }}
 		</div>
-		<div class='col-xs-2 form-group required'></div>
-		<div class='col-xs-6 form-group required'></div>
-	</div>
+	  </div>
 
 	<br />
 
 	<!-- Origin -->
 	<div class='form-row'>
 		<div class='col-xs-9 form-group required'>
-			<label class='control-label'>Where you are flying from</label>
-			<select name="intFrom" id="intFrom" class='form-control'>
-				<option value="">Where you are flying from</option>
-				
-			</select>
+		{{ Form::select('from', ['from'=>'From'],'', ['class'=>'form-control', 'name'=>'from', 'id'=>'from']) }}       
 		</div>
 	</div>
 
 	<!-- Destination -->
 	<div class='form-row'>
 		<div class='col-xs-9 form-group required'>
-			<label class='control-label'>Where you are flying to</label>
-			<select name="intTo" id="intTo" class='form-control'>
-				<option value="">Where you are flying to</option>
-				
-			</select>
+			{{ Form::select('to', ['to'=>'To'],'', ['class'=>'form-control', 'name'=>'to', 'id'=>'to']) }}       
 		</div>
 	</div>
 
 	<!-- Date Picker Departure Date -->
 	<div class='form-row'>
 		<div class='col-xs-9 form-group required'>
-			<label class='control-label'>Departing Date</label>
-			<input autocomplete='off' class='form-control' name="intDepart" id="intDepart" size='20' type='text'>
+			{{ Form::text('departure','',['placeholder'=>'Departure', 'autocomplete'=>'off', 'class'=>'form-control', 'name'=>'intDepart', 'id'=>'intDepart', 'size'=>'20']) }}
 		</div>
 	</div>
 
 	<!-- Date Picker Return Date -->
 	<div class='form-row'>
 		<div class='col-xs-9 form-group returnDate required'>
-			<label class='control-label'>Returning Date</label>
-			<input autocomplete='off' class='form-control' name="intReturn" id="intReturn" size='20' type='text'>
+			{{ Form::text('return','',['placeholder'=>'Return', 'autocomplete'=>'off', 'class'=>'form-control', 'name'=>'intReturn', 'id'=>'intReturn', 'size'=>'20']) }}
 		</div>
 	</div>
 
 	<!-- People? -->
 	<div class='form-row'>
 		<div class='col-xs-4 form-group card required'>
-			<label class='control-label'>Adult</label>
+		{{ Form::label('adult','Adult', ['class'=>'control-label']) }}
 			<select name="intAdults" id="intAdults" class='form-control'>
-				<?php for($i=1; $i<=9; $i++) { ?>
+				<?php for($i=1; $i<=7; $i++) { ?>
 				<option value="<?= $i ?>"><?= $i ?></option>
 				<?php } ?>
 			</select>
 		</div>
 		<div class='col-xs-4 form-group card required'>
-			<label class='control-label'>Child<small> (2-11)</small></label>
+		{{ Form::label('child','Child (below 12 years)', ['class'=>'control-label']) }}
 			<select name="intChildren" id="intChildren" class='form-control'>
 				<?php for($i=0; $i<=4; $i++) { ?>
 				<option value="<?= $i ?>"><?= $i ?></option>
@@ -110,5 +98,6 @@
 		</div>
 	</div>
 
+	{{ Form::close() }}
 </div> <!-- /container -->
 @endsection
